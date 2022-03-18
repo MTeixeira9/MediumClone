61974d50451c8ecaf825cada9f2bd42f3395b5ee
MTeixeira9 <mario_ze16@hotmail.com>
2022-03-18 11:40:10 +0000

    Add request effects

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 import { StoreModule } from '@ngrx/store'
+import { EffectsModule } from '@ngrx/effects'
 
 import { RegisterComponent } from 'src/app/auth/components/register/register.component'
 import { reducers } from 'src/app/auth/store/reducer'
+import { AuthService } from 'src/app/auth/services/auth.service'
+import { RegisterEffect } from 'src/app/auth/store/effects/register.effect'
 
 const routes = [
   {
@@ -20,7 +23,9 @@ const routes = [
     RouterModule.forChild(routes),
     ReactiveFormsModule,
     StoreModule.forFeature('auth', reducers),
+    EffectsModule.forFeature([RegisterEffect]),
   ],
   declarations: [RegisterComponent],
+  providers: [AuthService],
 })
 export class AuthModule {}
